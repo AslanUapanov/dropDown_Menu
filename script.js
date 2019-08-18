9a83c9c5391f86379fc9ecc086ef9ecaa7b3cc67
Aslan Uapanov <46054091+AslanUapanov@users.noreply.github.com>
2019-08-18 19:55:03 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let el = document.querySelectorAll('.menu-item');
+// for(let i = 0; i < el.length; i++){
+//     el[i].addEventListener('mouseenter', showSub, false);
+//     el[i].addEventListener('mouseleave', hideSub, false);
+// }
+el.forEach(function(elements, i, el){
+    el[i].addEventListener('mouseenter', showSub, false);
+    el[i].addEventListener('mouseleave', hideSub, false);
+});
+function showSub(){
+    if(this.children.length > 1){
+        this.children[1].style.height = 'auto';
+        this.children[1].style.opacity = '1';
+        this.children[1].style.overflow = 'visible';
+    }else{
+        return false;
+    }
+}
+function hideSub(){
+    if(this.children.length > 1){
+        this.children[1].style.height = '0';
+        this.children[1].style.opacity = '0';
+        this.children[1].style.overflow = 'hidden';
+    }else{
+        return false;
+    }
+}
